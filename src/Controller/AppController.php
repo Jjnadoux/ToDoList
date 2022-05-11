82e4ecfbcd09a0885a1acebc026a9d86c2ae9327
nalbrespic <nalbrespic@tessi.fr>
2022-05-11 16:48:57 +0200

    ok

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AppController extends AbstractController
 {
@@ -35,9 +36,42 @@ class AppController extends AbstractController
     {
         $list = $em->getRepository(ToDoList::class)->find($id);
         $listDetail = $em->getRepository(Task::class)->findAllTasksByList($id);
+        dump($listDetail);
         return $this->render('toDolistDetail.html.twig', [
             'list' => $list,
             'listDetail' => $listDetail,
         ]);
+    }
+
+    /**
+     * 
+     * @Route("/task/{id}/changeStatut", name="task_changeStatut")
+     * @param integer $id
+     */
+    public function ChangeDone(EntityManagerInterface $em, $id, Request $request)
+    {
+        $statut = $request->get('statut');
+        $task = $em->getRepository(Task::class)->find($id);
+        $task->setIsDone($statut);
+        $em->flush();
+
+        return new JsonResponse('ok');
+    }
+
+    /**
+     * @Route("/task/add", name="task_add")
+     */
+    public function AddTask(EntityManagerInterface $em, Request $request)
+    {
+        $title = $request->get('title');
+        $isDone = $request->get('isDone');
+        $list = $em->getRepository(ToDoList::class)->find($request->get('idlist'));
+
+        $newTask = new Task();
+        $newTask->setTitle($title);
+        $newTask->setIsDone($isDone);
+        $newTask->setIdToDoList($list);
+        $em->persist($newTask);
+        $em->flush();
     }
 }
